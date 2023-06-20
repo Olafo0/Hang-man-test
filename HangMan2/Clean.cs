58bbdb70f722ce7e543ab18b2c3f7ce52cfa78b2
Olafo0 <89582698+Olafo0@users.noreply.github.com>
2023-06-20 11:22:41 +0100

    Create Clean.cs

---
+++ HangMan2/Clean.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangMan2
+{
+    public class Clean
+    {
+        public string word;
+
+        public Clean(string word)
+        {
+            this.word = word;
+
+        }
+
+
+    }
+}
